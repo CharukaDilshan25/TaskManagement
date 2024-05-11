6e12d71c587e7044a34bec1594977f53b782d4ce
CharukaDilshan25 <147806106+CharukaDilshan25@users.noreply.github.com>
2024-05-12 00:06:21 +0530

    LOgo onboard

---
+++ app/src/main/java/com/example/mad4taskapp/OnBoard1.kt
@@ -0,0 +1,11 @@
+package com.example.mad4taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoard1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board1)
+    }
+}
