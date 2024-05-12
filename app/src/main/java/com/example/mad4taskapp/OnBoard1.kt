49a71f7000d58014a8ca15f527ee400427801db4
CharukaDilshan25 <147806106+CharukaDilshan25@users.noreply.github.com>
2024-05-12 13:57:07 +0530

    interface

--- app/src/main/java/com/example/mad4taskapp/OnBoard1.kt
+++ app/src/main/java/com/example/mad4taskapp/OnBoard1.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import com.example.mad4taskapp.OnBoard2
 
 class OnBoard1 : AppCompatActivity() {
@@ -12,17 +13,13 @@ class OnBoard1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_board1)
 
-        // Find the button by its ID
-        // Find the ImageButton by its ID
-        val imageButton = findViewById<ImageButton>(R.id.imageView4)
 
-        // Set click listener on the ImageButton
-        imageButton.setOnClickListener {
-            // Define the intent to navigate to the next activity
-            val intent = Intent(this, OnBoard2::class.java)
-
-            // Start the next activity
+        var imageView4 = findViewById<ImageView>(R.id.imageView4)
+        imageView4.setOnClickListener {
+            var intent = Intent(this, OnBoard2::class.java)
             startActivity(intent)
+            finish()
         }
+
     }
 }
